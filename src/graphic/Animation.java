844345a35519ce0cdd49ec43c4290c81bc7dd18d
gugustofanboy123 <50833190+gugustofanboy123@users.noreply.github.com>
2019-06-08 09:53:58 +0200

    Add files via upload
    Vorwärts Bewegung (Spaghetti Code Version)

---
+++ src/graphic/Animation.java
@@ -0,0 +1,44 @@
+package graphic;
+
+
+import java.awt.*;
+import java.io.File;
+
+public class Animation {
+
+    private static File[] rechts = new File[16];
+    private static File[] links = new File[16];
+    private static char previous_key = '0';
+    private static int index =0;
+
+    public Animation() {
+        init();
+    }
+
+
+   private void init() {
+       for (int i = 1; i <=16 ; i++) {
+           rechts[i-1] =  new File(".\\Gugusto Graphics\\A_run\\run"+i+".png");
+           links[i-1] = new File((".\\Gugusto Graphics\\A_backwards\\backwards"+(17-i)+".png"));
+       }
+
+   }
+   public File update(char key){
+        switch (key) {
+            case 'd' :
+                if (previous_key == 'd' && index < 16) { return rechts[index++]; }
+                else index = 0; previous_key = 'd'; return rechts[index];
+
+            case 'a' :
+
+                if (previous_key == 'a' && index < 16) { return links[index++]; }
+               // else index = 0; previous_key = 'a'; return links[index];
+
+
+
+            default:
+                return new File(".\\Gugusto Graphics\\char_tmp.png");
+
+        }
+   }
+}
---
+++ src/graphic/Player.java
@@ -0,0 +1,83 @@
+package game.object;
+
+import game.Camera;
+import game.object.blocks.GoalBlock;
+import graphic.Animation;
+import input.KeyState;
+import util.Size;
+import util.Vector;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import graphic.Animation;
+
+public class Player extends DynamicGameObject {
+
+    private static final Size size = new Size(1, 1);
+    private static Image playerImage = null;
+    private static Animation run = new Animation();
+
+    static {
+        File file = new File(".\\Gugusto Graphics\\char_tmp.png");
+
+        try {
+            playerImage = ImageIO.read(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Player(Vector position){
+        super(position, size);
+    }
+
+    @Override
+    public void update(double delta){
+        super.update(delta);
+
+        int step = 5;
+        if (KeyState.isDown('w'))
+            boundingBox.getPosition().add(new Vector(0, - delta * step)); animation('w');
+        if (KeyState.isDown('s'))
+            boundingBox.getPosition().add(new Vector(0, delta * step));  animation('s');
+        if (KeyState.isDown('a'))
+            boundingBox.getPosition().add(new Vector(- delta * step, 0));  animation('a');
+        if (KeyState.isDown('d')) {
+            boundingBox.getPosition().add(new Vector(delta * step, 0));  animation('d');
+
+        }
+        if (KeyState.isDown(32)) { // Space
+            if (isOnGround()) getVelocity().setY(-18);
+        }
+    }
+
+    private void animation(char key){
+        try {
+            playerImage = ImageIO.read( run.update(key));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void draw(Graphics2D g2d, Camera camera) {
+        super.draw(g2d, camera);
+        g2d.drawImage(playerImage, getX(), getY(), getWidth(), getHeight(), null);
+    }
+
+    @Override
+    public void collision(GameObject other) {
+        super.collision(other);
+        if (other instanceof GoalBlock) {
+            System.out.println("Jo du hast das Ziel erreicht!");
+        }
+    }
+
+    public void reset(){
+        setOnGround(true);
+        setVelocity(new Vector());
+    }
+
+}
