11537d09859dd726b53b9559dc4940c0952b3dbb
Moritz Beck (Birkenstab) <git@birkenstab.de>
2019-06-18 09:32:19 +0200

    Coin: Fix performance problem by calling SpriteSheet.extract only once

--- src/de/thu/gpro/gugusto/game/object/blocks/Coin.java
+++ src/de/thu/gpro/gugusto/game/object/blocks/Coin.java
@@ -11,14 +11,16 @@ import java.awt.image.BufferedImage;
 public class Coin extends AnimationBlock {
 
     private static final BufferedImage texture = TextureLoader.get(Texture.BLOCK_COIN);
+    private static final BufferedImage[] frames = SpriteSheet.extract(texture, 16, 1, 128, 128);
 
     private static SpriteAnimation createAnimation(){
-        BufferedImage[] frames = SpriteSheet.extract(texture, 16, 1, 128, 128);
         return new SpriteAnimation(frames, 1000);
     }
 
     public Coin(Vector position){
         super(BlockType.COIN, position, createAnimation(), false);
     }
+
+
 
 }
